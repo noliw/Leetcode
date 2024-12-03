9d2a528ad2bf99cd5288bf8edd1ee77637046ab2
Nolawi Workineh <95555715+noliw@users.noreply.github.com>
2024-12-03 02:28:15 -0800

    Time: 17 ms (34%), Space: 38.3 MB (6.56%) - LeetHub

---
+++ 1960-check-if-the-sentence-is-pangram/1960-check-if-the-sentence-is-pangram.kt
@@ -0,0 +1,27 @@
+/*
+1. What are our inputs:
+- a lower case string called sentence
+
+2. What do we need to do to the inputs:
+- check if the string is panagram meaning that every letter in the alphabet appears at least once
+
+
+3. what do we return:
+- return true if its a panagram and false if its not
+ */
+
+ /*
+ 
+  */
+
+
+class Solution {
+    fun checkIfPangram(sentence: String): Boolean {
+       val alphabets = ('a'..'z').toHashSet()
+       val sentenceLetters = hashSetOf<Char>()
+       for (letter in sentence){
+        sentenceLetters.add(letter)
+       }
+       return sentenceLetters == alphabets
+    }
+}
