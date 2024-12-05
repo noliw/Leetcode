e616b4389eac61984b62d1f862e7f90970c2eaba
Nolawi Workineh <95555715+noliw@users.noreply.github.com>
2024-12-05 01:55:58 -0800

    Time: 19 ms (75%), Space: 39.7 MB (62.96%) - LeetHub

---
+++ 2331-intersection-of-multiple-arrays/2331-intersection-of-multiple-arrays.kt
@@ -0,0 +1,32 @@
+/*
+1. inputs:
+    - 2D array nums
+    - the elements in the subarray are:
+        - positive
+        - distinct
+        - non-empty
+2. what do we need to do to that input:
+    - we need to find the numbers that are present in each subarray
+3. what do we return:
+    - we need to return list of integers sorted in ascending order
+*/
+
+
+class Solution {
+    fun intersection(nums: Array<IntArray>): List<Int> {
+       var numCount = mutableMapOf<Int, Int>().withDefault{ 0 }
+       var ans = mutableListOf<Int>()
+       for (subArrayIdx in nums){
+            for (numIdx in subArrayIdx) { 
+                numCount[numIdx] = numCount.getValue(numIdx) + 1
+            }
+       } 
+       for ((key, value) in numCount) {
+       if(value == nums.size){
+            ans.add(key)
+       }
+       }
+       ans.sort()
+       return ans
+    }
+}
