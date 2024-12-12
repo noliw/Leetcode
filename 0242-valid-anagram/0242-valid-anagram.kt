f8fc70195a8ba5d37407ea90919f114af2b183be
Nolawi Workineh <95555715+noliw@users.noreply.github.com>
2024-12-12 01:04:26 -0800

    Time: 19 ms (74.95%), Space: 38.5 MB (31.2%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.kt
+++ 0242-valid-anagram/0242-valid-anagram.kt
@@ -1,3 +1,19 @@
+/*
+1. inputs:
+    - two string s & t
+2. what do we do to those inputs:
+    - we need to compare and see if t is an anagra of s, making sure that all letters in t appear the same amount of times in s
+3. return a boolean
+
+ */
+
+ /*
+ the straightforward approach would be for every letter in t we iterate through s and see if it is present while keeping count, for this we may need to a nested loop, one for the outer and inner and this would be O(n^2) complexity and potentially O(1)
+
+ */
+
+
+
 class Solution {
     fun isAnagram(s: String, t: String): Boolean {
          val sSeen = HashMap<Char, Int>()
