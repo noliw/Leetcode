2a5982599b976c7801bc6218d1965e6de2f33784
Nolawi Workineh <95555715+noliw@users.noreply.github.com>
2025-02-18 04:02:49 -0800

    Time: 18 ms (79.73%), Space: 50.5 MB (92.67%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.kt
+++ 0217-contains-duplicate/0217-contains-duplicate.kt
@@ -20,22 +20,12 @@
 */
 class Solution {
     fun containsDuplicate(nums: IntArray): Boolean {
-        // Step 1: Create an empty set to store seen numbers
-    val seen = mutableSetOf<Int>()
-    
-    // Step 2: Loop through each number in the array
-    for (num in nums) {
-        // Step 3: Check if the number is already in the set
-        if (num in seen) {
-            // If the number is found, return true
-            return true
-        }
-        // Step 4: Add the number to the set
-        seen.add(num)
-    }
-    
-    // Step 5: If we reach here, no duplicates were found, so return false
-    return false
+       val seen = mutableSetOf<Int>()
 
+       for (num in nums){
+        if (num in seen) return true
+        seen.add(num)
+       }
+return false
     }
 }
