93008cf5d49e1768cbe66dc9cdf7bc3df1af8a3d
Nolawi Workineh <95555715+noliw@users.noreply.github.com>
2025-09-13 04:16:00 -0700

    Time: 6 ms (52.27%), Space: 57.8 MB (41.19%) - LeetHub

--- 1046-max-consecutive-ones-iii/1046-max-consecutive-ones-iii.kt
+++ 1046-max-consecutive-ones-iii/1046-max-consecutive-ones-iii.kt
@@ -1,16 +1,15 @@
 class Solution {
     fun longestOnes(nums: IntArray, k: Int): Int {
-        
         var left = 0
+        var zeroCount = 0
         var maxLength = 0
-        var numZeros = 0
 
-        for(right in nums.indices){
-            if(nums[right] == 0) numZeros++
-            while(numZeros > k){
-                 if(nums[left] == 0) numZeros--
+        for (right in nums.indices){
+            if(nums[right] == 0) zeroCount++
+
+            while(zeroCount > k){
+                if(nums[left] == 0) zeroCount--
                 left++
-               
             }
             maxLength = maxOf(maxLength, right - left + 1)
         }
