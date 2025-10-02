716d2e0a741e3ab34bc9d96a77c5b1cb763c0a29
Nolawi Workineh <95555715+noliw@users.noreply.github.com>
2025-10-01 20:56:16 -0700

    Time: 6 ms (70.74%), Space: 46.1 MB (45.48%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.kt
+++ 0125-valid-palindrome/0125-valid-palindrome.kt
@@ -1,17 +1,16 @@
 class Solution {
     fun isPalindrome(s: String): Boolean {
         val formatted = s.lowercase().filter { it.isLetterOrDigit() }
-        var right = formatted.length - 1
         var left = 0
-        
+        var right = formatted.length - 1
 
-        while (left < right){
-            if (formatted[left] != formatted[right]) return false
-                left++
-                right--
-            
+        while (left <= right){
+            if(formatted[right] != formatted[left]) return false
+            left++
+            right--
         }
         return true
+       
         
     }
 }
