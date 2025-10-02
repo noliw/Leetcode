0533bd1684d0a4efdc1bd7adfd3def676a3fcb16
Nolawi Workineh <95555715+noliw@users.noreply.github.com>
2025-10-01 21:56:48 -0700

    Time: 0 ms (100%), Space: 52.3 MB (81.58%) - LeetHub

---
+++ 0344-reverse-string/0344-reverse-string.kt
@@ -0,0 +1,16 @@
+class Solution {
+    fun reverseString(s: CharArray): Unit {
+        var left  = 0
+        var right = s.size - 1
+        var temp: Char
+
+        while (left < right){
+            temp = s[left]
+            s[left] = s[right]
+            s[right] = temp   
+            left++
+            right--  
+        }
+    
+    }
+}
