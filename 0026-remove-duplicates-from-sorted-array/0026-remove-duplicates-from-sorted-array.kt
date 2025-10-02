7aabb71413a1e0a417699be53b2026f7a70b335c
Nolawi Workineh <95555715+noliw@users.noreply.github.com>
2025-10-01 22:57:56 -0700

    Time: 1 ms (100%), Space: 53.9 MB (5%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.kt
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.kt
@@ -2,16 +2,14 @@ class Solution {
     fun removeDuplicates(nums: IntArray): Int {
         if (nums.isEmpty()) return 0
 
-    var write = 1  // index to place the next unique element
-
-    for (read in 1 until nums.size) {
-        if (nums[read] != nums[read - 1]) {
-            nums[write] = nums[read]
-            write++
+    var L = 0
+    for (R in 1 until nums.size) {
+        if (nums[R] != nums[L]) {
+            L++
+            nums[L] = nums[R]
         }
     }
-
-    return write
+    return L + 1
         
     }
 }
