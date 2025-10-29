79a06672b22cd0405aa2182e90b782fbf931cb82
Nolawi Workineh <95555715+noliw@users.noreply.github.com>
2025-10-28 21:46:50 -0700

    Time: 2 ms (96.63%), Space: 50.2 MB (86.22%) - LeetHub

--- 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.kt
+++ 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.kt
@@ -1,16 +1,20 @@
 class Solution {
     fun twoSum(numbers: IntArray, target: Int): IntArray {
-        var left = 0
-        var right = numbers.size - 1
+        if (numbers.isEmpty()) intArrayOf()
+       var left = 0
+       var right = numbers.size - 1
 
-        while (left <= right){
-            when{
-                numbers[left] + numbers[right] == target -> return intArrayOf( left + 1, right + 1)
-                numbers[left] + numbers[right] > target -> right--
-                else -> left++
-            }
+       while (left <= right){
+        when {
+            numbers[left] + numbers[right] == target -> return intArrayOf(left + 1, right + 1)
+            numbers[left] + numbers[right] > target -> right--
+            else -> left++
         }
-         return intArrayOf()
+
+
+
+       } 
+       
+        return intArrayOf()
     }
-   
 }
