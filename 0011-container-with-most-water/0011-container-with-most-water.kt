50a986635248d7d49587692d9a8e6288ac916546
Nolawi Workineh <95555715+noliw@users.noreply.github.com>
2025-11-05 05:26:24 -0800

    Time: 6 ms (79.95%), Space: 60.6 MB (29.68%) - LeetHub

--- 0011-container-with-most-water/0011-container-with-most-water.kt
+++ 0011-container-with-most-water/0011-container-with-most-water.kt
@@ -1,20 +1,19 @@
 class Solution {
     fun maxArea(height: IntArray): Int {
+        if(height.isEmpty()) return 0
         var left = 0
         var right = height.size - 1
         var maxArea = 0
 
-        while(left < right){
-            val currArea = minOf(height[left], height[right]) * (right - left)
-            maxArea = maxOf(currArea, maxArea)
-
-            if( height[left] < height[right]){
-                left++
-            } else {
-                 right--
-            }
+        while (left < right){
+            val width = right - left
+            val length = if (height[left] < height[right]) height[left] else height[right]
+            var currArea = width * length
             maxArea = maxOf(maxArea, currArea)
+
+             if(height[left] > height[right]) right-- else left++
         }
+
         return maxArea
     }
 }
